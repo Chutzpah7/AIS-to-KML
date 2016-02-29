112264db86214329dab7d67b26a7ce79cb4e0743
Logan Bown <loganpatrick99@gmail.com>
2016-02-28 22:33:20 -0600

    added options

--- AISDecoder.java
+++ AISDecoder.java
@@ -2,18 +2,22 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 public class AISDecoder {
-	public static final int KML = 0;
-	public static final int CSV = 1;
+	public static final int NOWRITE = 0;
+	public static final int KML = 1;
+	public static final int CSV = 2;
 
 
 	// Need a place to hold fragments of incomplete sentances
 	public static ArrayList<String> fragments = new ArrayList<String>();
+	public static ArrayList<Ship> shipArrl = new ArrayList<Ship>();
 	public static ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
 	public static boolean silent = false;
 	public static ArrayList<String> inputFiles = new ArrayList<String>();
 	public static String outputFile = "";
-	public static int outputType = KML;
-
+	public static int outputType = NOWRITE;
+	public static boolean filter = false;
+	public static String filterFile= "";
+	public static ArrayList<String> filteredMmsi = new ArrayList<String>();
 	// Takes an argument of a filename, crefates buffered reader 
 	public static void main(String[] args) {
 		//Holds information on all of the points to be written to KML
@@ -22,7 +26,9 @@ public class AISDecoder {
 		// Using try to handle I/O exceptions
 		// dataBR reads from file given as an argument to program
 		if (args.length == 0) {
-			System.out.println("Usage: AISDecoder [OPTION]... FILE\nOptions:\n\t-s, --silent\t\tdo not write anything to stdout\n\t-o, --output FILE\twrite output to a file\n\t-k, --kml\t\toutput as kml file (default)\n\t-c, --csv\t\toutput as csv file");
+			System.out.println("Usage: AISDecoder [OPTION]... FILES\nOptions:\n\t-s, --silent\t\tdo not write decoded data to stdout\n\t-o, --output FILE\twrite output to a file" +
+				"\n\t-k, --kml\t\toutput as kml file\n\t-c, --csv\t\toutput as csv file\n\nOptions for kml:\n\t-f, --filter FILE\t only include ships with MMSI specified " +
+				"in file");
 			System.exit(1);
 		}
 
@@ -34,18 +40,31 @@ public class AISDecoder {
 		}
 
 		for (int i = 0; i < args.length; i++) {
+			if (i != 0 && ((args[i-1].equalsIgnoreCase("-f") || args[i-1].equalsIgnoreCase("--filter"))) && !args[i].equalsIgnoreCase(outputFile)) {
+				filterFile = args[i];
+				break;
+			}
+		}
+
+		for (int i = 0; i < args.length; i++) {
 			if (args[i].charAt(0) == '-') {
 				switch(args[i].toLowerCase()) {
 					case "-s": case "--silent":
 						silent = true;
+						break;
+					case "-k": case "--kml":
+						outputType = KML;
 						break;
 					case "-c": case "--csv":
 						outputType = CSV;
 						break;
 					case "-o": case "--output":
 						break;
+					case "-f": case "--filter":
+						filter = true;
+						break;
 					default:
-						System.err.println("");
+						System.err.println("Invalid option" + args[i]);
 						System.exit(1);
 						break;
 				}
@@ -161,13 +180,13 @@ public class AISDecoder {
 			return;
 		}
 
-		long mmsi = getBits(8, 37, payload); //unsigned
+		long mmsi = getBits(8, 37, payload);
 
-		int navStatus = (int) getBits(38, 41, payload); //unsigned
+		int navStatus = (int) getBits(38, 41, payload);
 		String navStatusString;
 		navStatusString = navigationStatuses[navStatus];
 
-		long rateOfTurn = (getBits(42, 49, payload)); //signed scaled int
+		long rateOfTurn = (getBits(42, 49, payload));
 		String rateOfTurnString;
 		if (rateOfTurn == 0) {
 			rateOfTurnString = "0";
@@ -187,7 +206,8 @@ public class AISDecoder {
 		else
 			rateOfTurnString = "N/A";
 
-		long speedOverGround = getBits(50, 59, payload); //unsigned, scaled int
+
+		long speedOverGround = getBits(50, 59, payload);
 		String sogString;
 		if (speedOverGround == 1023)
 			sogString = "N/A";
@@ -196,9 +216,7 @@ public class AISDecoder {
 		else
 			sogString = "" + speedOverGround / 10.0;
 
-		boolean positionAccuracy = (getBits(60, 60, payload) == 1); // boolean
-
-		
+		boolean positionAccuracy = (getBits(60, 60, payload) == 1);
 
 		double longitude = (getBits(61, 61, payload) == 0) ? getBits(62, 88, payload) / 600000.0 : -1 * (((getBits(62, 88, payload)) ^ 0x7ffffff) - 1) / 600000.0; //2s compliment of unsigned part of longitude multiplied by sign bit
 		
@@ -208,12 +226,13 @@ public class AISDecoder {
 
 
 
-		dataPoints.add(new DataPoint(mmsi, String.format("%.13f",latitude), String.format("%.13f",longitude), navStatusString, rateOfTurnString, speedOverGround, positionAccuracy, courseOverGround));
+		dataPoints.add(new DataPoint(Long.toString(mmsi), String.format("%.5f",latitude), String.format("%.5f",longitude), navStatusString, rateOfTurnString, speedOverGround, positionAccuracy, courseOverGround));
 		return;
 	}
 	
 	public static int get6Bits(int index, String encodedData) {
-		return "0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW`abcdefghijklmnopqrstuvw".indexOf(encodedData.charAt(index)); // Payload decoding
+		// Index of characters in this string are equal to their 6-bit binary value for decoding
+		return "0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW`abcdefghijklmnopqrstuvw".indexOf(encodedData.charAt(index));
 	}
 
 	public static long getBits(int startBit, int endBit, String encodedData) {
@@ -235,28 +254,26 @@ public class AISDecoder {
 			output.createNewFile();
 			output.setWritable(true);
 			PrintWriter pw = new PrintWriter(output);
-			pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-			pw.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\">");
-			pw.println("<Document>");
-			pw.println("\t<name>" + outputFile + "</name>");
+			pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\nhttp://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n<Document>\n\t<name>" + outputFile + "</name>");
 			for (DataPoint dp : points) {
-				if(!silent)
-					System.out.println(dp);
-				pw.println("\t<Placemark>");
-				pw.println("\t\t<name>" + dp.mmsi + "</name>");
-				pw.println("\t\t<Style>");
-				pw.println("\t\t\t<IconStyle>");
-				pw.println("\t\t\t\t<heading>" + (int)dp.courseOverGround + "</heading>");
-				pw.println("\t\t\t\t<Icon>");
-				pw.println("\t\t\t\t\t<href>http://maps.google.com/mapfiles/kml/shapes/arrow.png</href>");
-				pw.println("\t\t\t\t</Icon>");
-				pw.println("\t\t\t</IconStyle>");
-				pw.println("\t\t</Style>");
-				pw.println("\t\t<Point>");
-				pw.println("\t\t\t<gx:drawOrder>1</gx:drawOrder>");
-				pw.println("\t\t\t<coordinates>" + dp.longitude + "," + dp.latitude+ ",0</coordinates>");
-				pw.println("\t\t</Point>");
-				pw.println("\t</Placemark>");
+				if (!(filter && filteredMmsi.contains(dp.mmsi)))
+					if (!silent)
+						System.out.println(dp);
+					pw.println("\t<Placemark>");
+					pw.println("\t\t<name>" + dp.mmsi + "</name>");
+					pw.println("\t\t<Style>");
+					pw.println("\t\t\t<IconStyle>");
+					pw.println("\t\t\t\t<heading>" + (int)dp.courseOverGround + "</heading>");
+					pw.println("\t\t\t\t<Icon>");
+					pw.println("\t\t\t\t\t<href>http://maps.google.com/mapfiles/kml/shapes/arrow.png</href>");
+					pw.println("\t\t\t\t</Icon>");
+					pw.println("\t\t\t</IconStyle>");
+					pw.println("\t\t</Style>");
+					pw.println("\t\t<Point>");
+					pw.println("\t\t\t<gx:drawOrder>1</gx:drawOrder>");
+					pw.println("\t\t\t<coordinates>" + dp.longitude + "," + dp.latitude+ ",0</coordinates>");
+					pw.println("\t\t</Point>");
+					pw.println("\t</Placemark>");
 			}
 			pw.println("</Document>");
 			pw.println("</kml>");
@@ -266,9 +283,18 @@ public class AISDecoder {
 	}
 }
 
+class Ship {
+	
+	public String mmsi;
+	public ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
+
+	public Ship(String mmsi){
+		this.mmsi = mmsi;
+	}
+}
 class DataPoint {
 
-	public long mmsi;
+	public String mmsi;
 	public String latitude;
 	public String longitude;
 	public String navStatus;
@@ -277,7 +303,7 @@ class DataPoint {
 	public String positionAccuracy;
 	public double courseOverGround;
 
-	public DataPoint(long mmsi, String latitude, String longitude, String navStatus, String rateOfTurn, long speedOverGround, boolean positionAccuracy, double courseOverGround) {
+	public DataPoint(String mmsi, String latitude, String longitude, String navStatus, String rateOfTurn, long speedOverGround, boolean positionAccuracy, double courseOverGround) {
 		this.mmsi = mmsi;
 		this.latitude = latitude;
 		this.longitude = longitude;
@@ -286,6 +312,19 @@ class DataPoint {
 		this.speedOverGround = (new Long(speedOverGround)).doubleValue() / 10.0;
 		this.positionAccuracy = positionAccuracy ? "DGPS-quality fix, accuracy < 10 m" : "GNSS fix, accuracy > 10m";
 		this.courseOverGround = courseOverGround;
+		boolean shipInArrl = false;
+
+		for(int i = 0; i < AISDecoder.shipArrl.size(); i++){
+			if (AISDecoder.shipArrl.get(i).mmsi == mmsi){
+				shipInArrl = true;
+				AISDecoder.shipArrl.get(i).dataPoints.add(this);
+				break;
+			}
+		}
+		if(!shipInArrl){
+			AISDecoder.shipArrl.add(new Ship(mmsi));
+			AISDecoder.shipArrl.get(AISDecoder.shipArrl.size()-1).dataPoints.add(this);
+		}
 	}
 
 	public String toString() {
